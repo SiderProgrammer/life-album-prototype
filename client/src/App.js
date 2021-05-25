44f0fb0a412938d6389a356d64ffd536ea86cd70
Sider <patryk.murawski.developer@gmail.com>
2021-05-25 14:21:16 +0200

    Revert "Revert "Revert "update"""
    This reverts commit ce483e0f2d20ac6075a8a5e6958658b6a4d2edf3.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import { CssBaseline } from "@material-ui/core";
 import NavBar from "./components/NavBar";
-
+import Post from "./components/Post";
 import Profile from "./components/Profile";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import Home from "./components/Home";
+import { BrowserRouter, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <CssBaseline />
-
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/signIn" component={SignIn} />
-          <Route exact path="/signUp" component={SignUp} />
-          <NavBar />
-        </Switch>
+        <NavBar />
 
-        <Route exact path="/" component={Home} />
+        <Route exact path="/">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              gap: "70px",
+            }}
+          >
+            <Post />
+            <Post />
+          </div>
+        </Route>
 
         <Route path="/profile" exact component={Profile} />
       </BrowserRouter>
